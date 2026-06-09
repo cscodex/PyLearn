62af3b9c379f9a04d656cd2e4f43d886de5c35cb
Preet Singh <Preetsingh921@gmail.com>
2026-06-09 16:44:49 +0530

    fix: adjust font sizes per feedback

--- backend/app/services/certificate_service.py
+++ backend/app/services/certificate_service.py
@@ -65,10 +65,10 @@ def generate_certificate_image(
     draw.rectangle([70, 70, width-70, height-70], outline=gold_color, width=1)
 
     # --- Fonts ---
-    title_font = _get_font("Roboto-Bold.ttf", 90)
+    title_font = _get_font("Roboto-Bold.ttf", 70)
     subtitle_font = _get_font("Roboto-Bold.ttf", 25)
-    name_font = _get_font("GreatVibes-Regular.ttf", 160)
-    course_font = _get_font("Roboto-Bold.ttf", 110)
+    name_font = _get_font("GreatVibes-Regular.ttf", 100)
+    course_font = _get_font("Roboto-Bold.ttf", 70)
     stat_font = _get_font("Roboto-Regular.ttf", 20)
     stat_bold_font = _get_font("Roboto-Bold.ttf", 20)
     signature_font = _get_font("GreatVibes-Regular.ttf", 60)
